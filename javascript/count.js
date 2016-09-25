8ec046d569bf89b813c945d79025bab9db403dd6
keiga <keiga@tidestar.jp>
2016-09-25 15:34:32 +0900

    パラメタを設定

--- javascript/count.js
+++ javascript/count.js
@@ -2,6 +2,9 @@
 
 	"use strict";
 
-	console.log("hogehoge");
+	var textItem = document.body.getElementsByTagName("text");
+	var numItem = document.body.getElementsByTagName("num");
+
+	num.innerText = "0";
 
 })();
