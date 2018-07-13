33c8ddb1ff99f6b93da22c66855bcce47e68ca5b
keiga <keiga@tidestar.jp>
2018-07-13 20:39:00 +0900

    varは使わない

--- javascript/count.js
+++ javascript/count.js
@@ -2,11 +2,11 @@
 
 	"use strict";
 
-	var COUNT_MAX = 145;
-	var COUNT_MIN = 125;
+	const COUNT_MAX = 145;
+	const COUNT_MIN = 125;
 
-	var textItem = document.getElementById("text");
-	var numItem = document.getElementById("num");
+	let textItem = document.getElementById("text");
+	let numItem = document.getElementById("num");
 
 	countText();
 
@@ -14,9 +14,9 @@
 	textItem.addEventListener("keyup", countText);
 
 	function countLength(str) {
-		var r = 0;
-		for (var i = 0; i < str.length; i++) {
-			var c = str.charCodeAt(i);
+		let r = 0;
+		for (let i = 0; i < str.length; i++) {
+			let c = str.charCodeAt(i);
 			// Shift_JIS: 0x0 ～ 0x80, 0xa0 , 0xa1 ～ 0xdf , 0xfd ～ 0xff
 			// Unicode : 0x0 ～ 0x80, 0xf8f0, 0xff61 ～ 0xff9f, 0xf8f1 ～ 0xf8f3
 			if ((c >= 0x0 && c < 0x81) || (c == 0xf8f0) || (c >= 0xff61 && c < 0xffa0) || (c >= 0xf8f1 && c < 0xf8f4)) {
@@ -29,7 +29,7 @@
 	}
 
 	function countText() {
-		var num = countLength(textItem.value);
+		let num = countLength(textItem.value);
 		setNum(num);
 	}
 
